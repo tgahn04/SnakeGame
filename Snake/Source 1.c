b41f61c32416ec7019efa60f3abd24ba88e25ca8
tgahn04 <tgahn0524@gmail.com>
2025-08-28 19:44:43 +0900

    (2025,8,28) Snake 4일차

--- Snake/Source 1.c
+++ Snake/Source 1.c
@@ -4,12 +4,12 @@
 #include <conio.h>
 #include <windows.h>
 
-#define WIDTH       100
-#define HEIGHT      50
-#define UP    72
-#define DOWN  80
-#define LEFT  75
-#define RIGHT 77
+#define WIDTH	100
+#define HEIGHT  25
+#define UP		72
+#define DOWN	80
+#define LEFT	75
+#define RIGHT	77
 
 int screen_index;
 
@@ -29,6 +29,13 @@ typedef struct
 	Position position[100];
 
 } Snake;
+
+typedef struct
+{
+	int x;
+	int y;
+
+} Food;
 
 void initialize()
 {
@@ -84,35 +91,96 @@ void release()
 	CloseHandle(screen[1]);
 }
 
-void render(Snake snake)
+void renderSnake(Snake snake)
 {
 	DWORD dword;
-	COORD position = { snake.position[0].x, snake.position[0].y };
-	char c = '@';
-	SetConsoleCursorPosition(screen[screen_index], position);
-	WriteFile(screen[screen_index], &c, 1, &dword, NULL);
+
+	COORD head_position = { snake.position[0].x, snake.position[0].y };
+	char head = '@';
+	SetConsoleCursorPosition(screen[screen_index], head_position);
+	WriteFile(screen[screen_index], &head, 1, &dword, NULL);
+	
+	for (int i = 1; i < snake.length; i++)
+	{
+		COORD body_position = { snake.position[i].x, snake.position[i].y };
+		char body = '0';
+		SetConsoleCursorPosition(screen[screen_index], body_position);
+		WriteFile(screen[screen_index], &body, 1, &dword, NULL);
+	}
 }
 
-// void Field(Snake snake, Position position)
-// {
-// 	for (int i = 0; i < HEIGHT; i++)
-// 	{
-// 		for (int j = 0; j < WIDTH; j++)
-// 		{
-// 
-// 		}
-// 	}
-// }
+void renderFood(Food food)
+{
+	DWORD dword;
 
+	COORD position = {food.x, food.y};
+	char f = '¢¾';
+	SetConsoleCursorPosition(screen[screen_index], position);
+	WriteFile(screen[screen_index], &f, 1, &dword, NULL);
+}
+
+ void renderField()
+ {
+	 DWORD dword;
+
+	 char wall = '#';
+	 char emp = ' ';
+
+ 	for (int i = 0; i < HEIGHT; i++)
+ 	{
+ 		for (int j = 0; j < WIDTH; j++)
+ 		{
+			COORD position = { j, i };
+
+			if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)
+			{
+				SetConsoleCursorPosition(screen[screen_index], position);
+				WriteFile(screen[screen_index], &wall, 1, &dword, NULL);
+			}
+			else
+			{
+				SetConsoleCursorPosition(screen[screen_index], position);
+				WriteFile(screen[screen_index], &emp, 1, &dword, NULL);
+			}
+ 		}
+ 	}
+ }
+
+ void SnakeMove(Snake snake)
+ {
+	 for (int i = snake.length - 1; i > 0; i--)
+	 {
+		 snake.position[i] = snake.position[i - 1];
+	 }
+
+	 switch (snake.direction)
+	 {
+	 case UP:    snake.position[0].y--; break;
+	 case DOWN:  snake.position[0].y++; break;
+	 case LEFT:  snake.position[0].x--; break;
+	 case RIGHT: snake.position[0].x++; break;
+	 }
+ }
+
+ void SpawnFood(Food* f)
+ {
+	 f->x = rand() % (WIDTH - 2) + 1;
+	 f->y = rand() % (HEIGHT - 2) + 1;
+ }
 
 int main()
 {
-	 Snake snake;
-	 snake.length = 1;
-	 snake.direction = RIGHT;
+	srand((unsigned int)time(NULL));
 
-	 snake.position[0].x = 10;
-	 snake.position[0].y = 10;
+	Snake snake;
+	snake.length = 3;
+	snake.direction = RIGHT;
+	snake.position[0].x = WIDTH / 2;
+	snake.position[0].y = HEIGHT / 2;
+	snake.position[1].x = WIDTH / 2 - 1;
+	snake.position[1].y = HEIGHT / 2;
+	snake.position[2].x = WIDTH / 2 - 2;
+	snake.position[2].y = HEIGHT / 2;
 
 	initialize();
 
@@ -121,35 +189,36 @@ int main()
 		if (_kbhit())
 		{
 			int key = _getch();
+
 			if (key == 0 || key == 224) key = _getch();
-			// switch (key)
-			// {
-			// 	case UP :  if (snake.direction != DOWN) snake.direction = UP; break;
-			// 	case LEFT: if (snake.direction != RIGHT) snake.direction = LEFT; break;
-			// 	case RIGHT: if (snake.direction != LEFT) snake.direction = RIGHT; break;
-			// 	case DOWN: if (snake.direction != UP) snake.direction = DOWN; break;
-			// }
-			// switch()
 
 			switch (key)
 			{
-			case UP:	 snake.direction = UP; break;
-			case LEFT:	 snake.direction = LEFT; break;
-			case RIGHT:	 snake.direction = RIGHT; break;
-			case DOWN:	 snake.direction = DOWN; break;
+				case UP :	if (snake.direction != DOWN) snake.direction = UP; break;
+				case LEFT:	if (snake.direction != RIGHT) snake.direction = LEFT; break;
+				case RIGHT: if (snake.direction != LEFT) snake.direction = RIGHT; break;
+				case DOWN:	if (snake.direction != UP) snake.direction = DOWN; break;
 			}
 
 			switch (snake.direction)
 			{
-				case UP:	 snake.position[0].y--; break;
-				case LEFT:	 snake.position[0].x--; break;
-				case RIGHT:	 snake.position[0].x++; break;
-				case DOWN:	 snake.position[0].y++; break;
+			case UP:    snake.position[0].y--; break;
+			case DOWN:  snake.position[0].y++; break;
+			case LEFT:  snake.position[0].x--; break;
+			case RIGHT: snake.position[0].x++; break;
 			}
+			
+			for (int i = snake.length; i > 0; i--)
+			{
+				snake.position[i] = snake.position[i - 1];
+			}
+			
+			
 		}
 		
 		clear();
-		render(snake);
+		renderField();
+		renderSnake(snake);
 		flip();
 	}
 	release();
--- Snake/Source.c
+++ Snake/Source.c
@@ -1,215 +1,215 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <conio.h>
-#include <windows.h>
-
-#define WIDTH       100
-#define HEIGHT      50
-#define MAX_Length  500
-
-#define UP    72
-#define DOWN  80
-#define LEFT  75
-#define RIGHT 77
-
-typedef struct 
-{
-    int x[MAX_Length];
-    int y[MAX_Length];
-    int length;
-} Snake;
-
-typedef struct 
-{
-    int x;
-    int y;
-    int active;
-} Food;
-
-
-CHAR_INFO buffer[WIDTH * HEIGHT];
-HANDLE hConsole;
-COORD bufferSize = { WIDTH, HEIGHT };
-COORD bufferCoord = { 0, 0 };
-SMALL_RECT writeRegion = { 0, 0, WIDTH - 1, HEIGHT - 1 };
-
-void InitConsoleBuffer()
-{
-    hConsole = CreateConsoleScreenBuffer(
-        GENERIC_READ | GENERIC_WRITE,
-        0,
-        NULL,
-        CONSOLE_TEXTMODE_BUFFER,
-        NULL
-    );
-    SetConsoleActiveScreenBuffer(hConsole);
-}
-
-void DrawBuffer() 
-{
-    WriteConsoleOutput(hConsole, buffer, bufferSize, bufferCoord, &writeRegion);
-}
-
-void Field(Snake snake, Food foods[], int foodCount) 
-{
-   
-    for (int i = 0; i < HEIGHT; i++)
-    {
-        for (int j = 0; j < WIDTH; j++) 
-        {
-            int idx = i * WIDTH + j;
-            buffer[idx].Char.AsciiChar = ' ';
-            buffer[idx].Attributes = 0x07;
-        }
-    }
-
-    for (int i = 0; i < HEIGHT; i++) 
-    {
-        for (int j = 0; j < WIDTH; j++) 
-        {
-            if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) 
-            {
-                buffer[i * WIDTH + j].Char.AsciiChar = '#';
-            }
-        }
-    }
-
-    for (int f = 0; f < foodCount; f++)
-    {
-        if (foods[f].active)
-        {
-            buffer[foods[f].y * WIDTH + foods[f].x].Char.AsciiChar = 'O';
-        }
-    }
-
-    for (int k = 0; k < snake.length; k++)
-    {
-        int x = snake.x[k];
-        int y = snake.y[k];
-        buffer[y * WIDTH + x].Char.AsciiChar = (k == 0) ? '@' : '0';
-    }
-
-    DrawBuffer();
-}
-
-void SpawnFood(Food* f) 
-{
-    f->x = rand() % (WIDTH - 2) + 1;
-    f->y = rand() % (HEIGHT - 2) + 1;
-    f->active = 1;
-}
-
-void SnakeMove(Snake* snake, int dx, int dy) 
-{
-    for (int i = snake->length; i > 0; i--)
-    {
-        snake->x[i] = snake->x[i - 1];
-        snake->y[i] = snake->y[i - 1];
-    }
-     snake->x[0] += dx;
-     snake->y[0] += dy;
-}
-
-int HitSelf(Snake snake)
-{
-    for (int i = 1; i < snake.length; i++) 
-    {
-        if (snake.x[0] == snake.x[i] && snake.y[0] == snake.y[i])
-            return 1;
-    }
-    return 0;
-}
-
-int main() 
-{
-    srand((unsigned int)time(NULL));
-
-        system("cls");
-        printf("---- Snake_Game ----\n\n");
-        printf("조작법 : 방향키\n\n");
-        printf("먹이를 3개 먹으면 길이 + 1\n");
-        printf("먹이 30개를 먹으면 게임이 클리어됩니다.\n");
-        printf("벽이나 자신의 몸에 부딪히면 게임오버입니다.\n\n");
-        printf("아무 키나 누르면 시작합니다.\n");
-
-        _getch();
-
-        InitConsoleBuffer();
-
-        Snake snake = { {WIDTH / 2}, {HEIGHT / 2}, 1 };
-        Food foods[1000] = { 0 };
-        int foodCount = 0;
-
-        int EatFood = 0;
-        int dx = 0, dy = -1;
-        int growthCount = 0;
-
-        DWORD lastFoodTime = GetTickCount();
-
-        while (1)
-        {
-            if (_kbhit())
-            {
-                int key = _getch();
-                if (key == 0 || key == 224) key = _getch();
-                switch (key) {
-                case UP:    if (dy != 1) { dx = 0; dy = -1; } break;
-                case DOWN:  if (dy != -1) { dx = 0; dy = 1; } break;
-                case LEFT:  if (dx != 1) { dx = -1; dy = 0; } break;
-                case RIGHT: if (dx != -1) { dx = 1; dy = 0; } break;
-                }
-            }
-
-            if (GetTickCount() - lastFoodTime >= 2000)
-            {
-                if (foodCount < 1000) SpawnFood(&foods[foodCount++]);
-                lastFoodTime = GetTickCount();
-            }
-
-            SnakeMove(&snake, dx, dy);
-
-            if (snake.x[0] <= 0 || snake.x[0] >= WIDTH - 1 ||
-                snake.y[0] <= 0 || snake.y[0] >= HEIGHT - 1)
-            {
-                system("cls");
-                printf("GAME_OVER\n");
-                break;
-            }
-
-            if (HitSelf(snake))
-            {
-                system("cls");
-                printf("GAME_OVER\n");
-                break;
-            }
-
-            for (int f = 0; f < foodCount; f++)
-            {
-                if (foods[f].active && snake.x[0] == foods[f].x && snake.y[0] == foods[f].y)
-                {
-                    foods[f].active = 0;
-                    EatFood++;
-                    growthCount++;
-
-                    if (growthCount == 3)
-                    {
-                        if (snake.length < MAX_Length) snake.length++;
-                        growthCount = 0;
-                    }
-                    if (EatFood >= 30)
-                    {
-                        system("cls");
-                        printf("GAME_CLEAR!\n총 점수: %d\n", EatFood);
-                        return 0;
-                    }
-                }
-            }
-
-
-            Field(snake, foods, foodCount);
-
-            Sleep(33);
-        }
-    return 0;
-}
+﻿// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <conio.h>
+// #include <windows.h>
+// 
+// #define WIDTH       100
+// #define HEIGHT      50
+// #define MAX_Length  500
+// 
+// #define UP    72
+// #define DOWN  80
+// #define LEFT  75
+// #define RIGHT 77
+// 
+// typedef struct 
+// {
+//     int x[MAX_Length];
+//     int y[MAX_Length];
+//     int length;
+// } Snake;
+// 
+// typedef struct 
+// {
+//     int x;
+//     int y;
+//     int active;
+// } Food;
+// 
+// 
+// CHAR_INFO buffer[WIDTH * HEIGHT];
+// HANDLE hConsole;
+// COORD bufferSize = { WIDTH, HEIGHT };
+// COORD bufferCoord = { 0, 0 };
+// SMALL_RECT writeRegion = { 0, 0, WIDTH - 1, HEIGHT - 1 };
+// 
+// void InitConsoleBuffer()
+// {
+//     hConsole = CreateConsoleScreenBuffer(
+//         GENERIC_READ | GENERIC_WRITE,
+//         0,
+//         NULL,
+//         CONSOLE_TEXTMODE_BUFFER,
+//         NULL
+//     );
+//     SetConsoleActiveScreenBuffer(hConsole);
+// }
+// 
+// void DrawBuffer() 
+// {
+//     WriteConsoleOutput(hConsole, buffer, bufferSize, bufferCoord, &writeRegion);
+// }
+// 
+// void Field(Snake snake, Food foods[], int foodCount) 
+// {
+//    
+//     for (int i = 0; i < HEIGHT; i++)
+//     {
+//         for (int j = 0; j < WIDTH; j++) 
+//         {
+//             int idx = i * WIDTH + j;
+//             buffer[idx].Char.AsciiChar = ' ';
+//             buffer[idx].Attributes = 0x07;
+//         }
+//     }
+// 
+//     for (int i = 0; i < HEIGHT; i++) 
+//     {
+//         for (int j = 0; j < WIDTH; j++) 
+//         {
+//             if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) 
+//             {
+//                 buffer[i * WIDTH + j].Char.AsciiChar = '#';
+//             }
+//         }
+//     }
+// 
+//     for (int f = 0; f < foodCount; f++)
+//     {
+//         if (foods[f].active)
+//         {
+//             buffer[foods[f].y * WIDTH + foods[f].x].Char.AsciiChar = 'O';
+//         }
+//     }
+// 
+//     for (int k = 0; k < snake.length; k++)
+//     {
+//         int x = snake.x[k];
+//         int y = snake.y[k];
+//         buffer[y * WIDTH + x].Char.AsciiChar = (k == 0) ? '@' : '0';
+//     }
+// 
+//     DrawBuffer();
+// }
+// 
+// void SpawnFood(Food* f) 
+// {
+//     f->x = rand() % (WIDTH - 2) + 1;
+//     f->y = rand() % (HEIGHT - 2) + 1;
+//     f->active = 1;
+// }
+// 
+// void SnakeMove(Snake* snake, int dx, int dy) 
+// {
+//     for (int i = snake->length; i > 0; i--)
+//     {
+//         snake->x[i] = snake->x[i - 1];
+//         snake->y[i] = snake->y[i - 1];
+//     }
+//      snake->x[0] += dx;
+//      snake->y[0] += dy;
+// }
+// 
+// int HitSelf(Snake snake)
+// {
+//     for (int i = 1; i < snake.length; i++) 
+//     {
+//         if (snake.x[0] == snake.x[i] && snake.y[0] == snake.y[i])
+//             return 1;
+//     }
+//     return 0;
+// }
+// 
+// int main() 
+// {
+//     srand((unsigned int)time(NULL));
+// 
+//         system("cls");
+//         printf("---- Snake_Game ----\n\n");
+//         printf("조작법 : 방향키\n\n");
+//         printf("먹이를 3개 먹으면 길이 + 1\n");
+//         printf("먹이 30개를 먹으면 게임이 클리어됩니다.\n");
+//         printf("벽이나 자신의 몸에 부딪히면 게임오버입니다.\n\n");
+//         printf("아무 키나 누르면 시작합니다.\n");
+// 
+//         _getch();
+// 
+//         InitConsoleBuffer();
+// 
+//         Snake snake = { {WIDTH / 2}, {HEIGHT / 2}, 1 };
+//         Food foods[1000] = { 0 };
+//         int foodCount = 0;
+// 
+//         int EatFood = 0;
+//         int dx = 0, dy = -1;
+//         int growthCount = 0;
+// 
+//         DWORD lastFoodTime = GetTickCount();
+// 
+//         while (1)
+//         {
+//             if (_kbhit())
+//             {
+//                 int key = _getch();
+//                 if (key == 0 || key == 224) key = _getch();
+//                 switch (key) {
+//                 case UP:    if (dy != 1) { dx = 0; dy = -1; } break;
+//                 case DOWN:  if (dy != -1) { dx = 0; dy = 1; } break;
+//                 case LEFT:  if (dx != 1) { dx = -1; dy = 0; } break;
+//                 case RIGHT: if (dx != -1) { dx = 1; dy = 0; } break;
+//                 }
+//             }
+// 
+//             if (GetTickCount() - lastFoodTime >= 2000)
+//             {
+//                 if (foodCount < 1000) SpawnFood(&foods[foodCount++]);
+//                 lastFoodTime = GetTickCount();
+//             }
+// 
+//             SnakeMove(&snake, dx, dy);
+// 
+//             if (snake.x[0] <= 0 || snake.x[0] >= WIDTH - 1 ||
+//                 snake.y[0] <= 0 || snake.y[0] >= HEIGHT - 1)
+//             {
+//                 system("cls");
+//                 printf("GAME_OVER\n");
+//                 break;
+//             }
+// 
+//             if (HitSelf(snake))
+//             {
+//                 system("cls");
+//                 printf("GAME_OVER\n");
+//                 break;
+//             }
+// 
+//             for (int f = 0; f < foodCount; f++)
+//             {
+//                 if (foods[f].active && snake.x[0] == foods[f].x && snake.y[0] == foods[f].y)
+//                 {
+//                     foods[f].active = 0;
+//                     EatFood++;
+//                     growthCount++;
+// 
+//                     if (growthCount == 3)
+//                     {
+//                         if (snake.length < MAX_Length) snake.length++;
+//                         growthCount = 0;
+//                     }
+//                     if (EatFood >= 30)
+//                     {
+//                         system("cls");
+//                         printf("GAME_CLEAR!\n총 점수: %d\n", EatFood);
+//                         return 0;
+//                     }
+//                 }
+//             }
+// 
+// 
+//             Field(snake, foods, foodCount);
+// 
+//             Sleep(33);
+//         }
+//     return 0;
+// }
